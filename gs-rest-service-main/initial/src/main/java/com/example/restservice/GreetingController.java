95999cb8dbf0842c98e807901d5fbb869c898a31
Matthew Perez <matthew.perez413@gmail.com>
2022-12-29 18:23:49 -0600

    ServiceSetUp

---
+++ gs-rest-service-main/initial/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/")
+    public String getGreeting () {
+        return "Hello World!";
+    }
+}
